2fd144e265772d58432e9feddcddf3423fa1ed58
hoanpham17 <hoan1012.utc@gmail.com>
2024-05-14 23:50:33 +0200

    --

--- lib/page/weather_service.dart
+++ lib/page/weather_service.dart
@@ -3,9 +3,9 @@ import 'package:http/http.dart' as http;
 
 class WeatherService {
   final String baseUrl = 'https://api.openweathermap.org/data/2.5';
-  final String apiKey = '81b7e6fe9a73494875ec45c68254eba5';
+  final String apiKey = '';
   Future<dynamic> fetchWeather(String city) async {
-    final response = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=London&appid=81b7e6fe9a73494875ec45c68254eba5'));
+    final response = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=London&appid='));
     if (response.statusCode == 200) {
       return json.decode(response.body);
     } else {
